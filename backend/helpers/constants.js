74f30cf02683a96ef8254682583f522b13e25288
Nick N <nick.r.norton@gmail.com>
2023-11-03 21:47:37 -0600

    fix: send 404 resposnes as JSON
    Move the 404 message into a JSON response constant and return
    that when something isn't found.

--- backend/helpers/constants.js
+++ backend/helpers/constants.js
@@ -3,9 +3,15 @@ const errorResponse = {
     message: "Something went wrong."
 };
 
+const notFoundResponse = {
+    success: false,
+    message: "Endpoint does not exist."
+};
+
 const HttpCode = Object.freeze({
     OK: 200,
+    NOT_FOUND: 404,
     SERVER_ERROR: 500
 });
 
-module.exports = { errorResponse, HttpCode };
+module.exports = { errorResponse, notFoundResponse, HttpCode };
